982fb41674044ea680101bfee0eee79720d5bec5
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2022-01-14 12:25:00 -0800

    Basic communication between Node.js and React

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './avistad-frontend/build')));
 
 app.get('/api', (req, res) => {
     res.json({message: 'Hello world!'});
 });
-
-const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`[*] Backend server listening on :${PORT}...`);
