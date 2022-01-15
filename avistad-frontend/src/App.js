ef76331df83dd6370cf91f58016258cda1a2093d
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2022-01-15 15:56:46 -0800

    Add Google Maps API integration

--- avistad-frontend/src/App.js
+++ avistad-frontend/src/App.js
@@ -1,9 +1,18 @@
+import MapSection from './components/map/Map';
+
 import './App.css';
+
+const location = {
+    address: '1923 N Hamilton St, Spokane, WA 99207',
+    lat: 47.6754785,
+    lng: -117.3968262 
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <MapSection location={location} zoomLevel={17}/>
     </div>
   );
 }
