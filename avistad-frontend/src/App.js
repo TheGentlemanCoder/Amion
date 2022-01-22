169de74031a528a67f8245f1db623bfc6930c7b4
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2022-01-22 01:44:24 -0800

    Modify frontend to use google-map-react package and utilize demo

--- avistad-frontend/src/App.js
+++ avistad-frontend/src/App.js
@@ -1,18 +1,11 @@
-import MapSection from './components/map/Map';
-
+import React from 'react';
 import './App.css';
-
-const location = {
-    address: '1923 N Hamilton St, Spokane, WA 99207',
-    lat: 47.6754785,
-    lng: -117.3968262 
-}
+import MyGoogleMap from './components/MyGoogleMap';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-      <MapSection location={location} zoomLevel={17}/>
+    <div className="main-wrapper">
+	<MyGoogleMap/>
     </div>
   );
 }
---
+++ avistad-frontend/src/components/Autocomplete.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    position: relative;
+    align-itmes: center;
+    justify-content: center;
+    width: 100%;
+    padding: 20px;
+    text-align: center;
+`;
+
+class AutoComplete extends Component {
+    constructor (props) {
+        super(props);
+        this.clearSearchBox = this.clearSearchBox.bind(this);
+    }
+
+    componentDidMount({ map, mapApi } = this.props) {
+        const options = {
+            // restrict your search to a specific type of result
+            types: ['address'],
+            // restrict your search to a specific country or an array of countries
+            componentRestrictions: { country: ['us', 'az'] }
+        };
+
+        this.autoComplete = new mapApi.places.Autocomplete(
+            this.searchInput,
+            options
+        );
+
+        this.autoComplete.addListener('place_changed', this.onPlaceChanged);
+        this.autoComplete.bindTo('bounds', map);
+    }
+
+    componentWillUnmount({ mapApi } = this.props) {
+        mapApi.event.clearInstanceListeners(this.searchInput);
+    }
+
+    onPlaceChanged = ({ map, addplace } = this.props) => {
+        const place = this.autoComplete.getPlace();
+
+        if (!place.geomerty) {
+            return;
+        }
+
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+
+        addplace(place);
+        this.searchInput.blur();
+    };
+
+    clearSearchBox() {
+        this.searchInput.value = '';
+    }
+
+    render() {
+        return (
+            <Wrapper>
+                <input
+                    className="search-input"
+                    ref={(ref) => {
+                        this.searchInput = ref;
+                    }}
+                    type="text"
+                    onFocus={this.clearSearchBox}
+                    placeholder="Enter your location"
+                />
+            </Wrapper>
+        );
+    }
+}
+
+export default AutoComplete;
